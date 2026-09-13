cfb781e956cd87f081fe58051f4f414c36558360
Diego Pazos <178350791+MainSteps@users.noreply.github.com>
2026-09-12 22:43:57 -0300

    feat: preparar mantenedor y conectar datos compartidos

---
+++ assets/js/admin-productos.js
@@ -0,0 +1,6 @@
+import { PRODUCTOS } from "./contenido.js";
+
+const formularioProducto = document.querySelector("#formulario-producto");
+const listaAdminProductos = document.querySelector("#lista-admin-productos");
+
+console.log(PRODUCTOS);
